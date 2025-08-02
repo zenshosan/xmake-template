fcc2a26ddea8b21cf65c5d4ca4fa45553c85dda9
Masaaki HAMADA <msk24000@outlook.com>
2025-08-02 09:23:14 +0900

    Fixed inconsistent extern use

--- include/mylib.h
+++ include/mylib.h
@@ -1,11 +1,2 @@
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-    const char *get_version(void);
-    int myadd(int x, int y);
-
-#ifdef __cplusplus
-}
-#endif
+const char *get_version(void);
+int myadd(int x, int y);
